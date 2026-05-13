1ab833e865d5d283eae1be013d1d1abadde4e4ce
chandanjha333 <162859024+chandanjha333@users.noreply.github.com>
2026-05-14 02:35:13 +0530

    Practiced REST APIs for CRUD application

--- backend-practices/server/index.js
+++ backend-practices/server/index.js
@@ -1,8 +1,11 @@
-import http from 'http';
-
 import express from 'express';
+import fs from 'fs';
+import users from '../MOCK_DATA.json' with { type: "json" };
 
 const app = express();
+
+//Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   return res.send('Hello from HomePage');
@@ -10,6 +13,57 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   return res.send('Hello from AboutPage ' + 'hey' + req.query.name);
+});
+
+//REST API
+app.get('/api/users', (req, res) => {
+  return res.json(users);
+});
+
+app.get('/users', (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
+
+  res.send(html);
+});
+
+app
+  .route('/api/users/:id')
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    Object.assign(user, req.body);
+    fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if(err) console.log(err);
+      return res.json({ status: "success" });
+    }); 
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const updatedUsers = users.filter(user => user.id !== id);
+    //TODO: Delete the user with id
+    fs.writeFile("../MOCK_DATA.json", JSON.stringify(updatedUsers), (err) => {
+      if(err) console.log(err);
+      return res.json({ status: "success" });
+    });
+  });
+
+
+app.post('/api/users', (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if(err) console.log(err);
+    return res.json({ status: "success", id: users.length });
+  }); 
 });
 
 app.listen(8000, () => {
